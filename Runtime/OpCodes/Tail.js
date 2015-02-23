f4eea4e58ac96ae8a0ff5829d50d23f36dc7e09f
Shmueli Englard <shmueli.yosef@englard.net>
2015-02-23 18:54:15 -0500

    Added tail opcode

---
+++ Runtime/OpCodes/Tail.js
@@ -0,0 +1,30 @@
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var CIL;
+(function (CIL) {
+    (function (Runtime) {
+        var Tail = (function (_super) {
+            __extends(Tail, _super);
+            function Tail(memory, stack) {
+                _super.call(this, memory, stack);
+            }
+            Tail.prototype.number = function () {
+                return 65044;
+            };
+
+            Tail.prototype.argumentCount = function () {
+                return [];
+            };
+            return Tail;
+        })(Runtime.OpCode);
+        Runtime.Tail = Tail;
+
+        Runtime.OpCode.opCodes[Tail.prototype.number()] = Tail;
+    })(CIL.Runtime || (CIL.Runtime = {}));
+    var Runtime = CIL.Runtime;
+})(CIL || (CIL = {}));
+//# sourceMappingURL=Tail.js.map
---
+++ Runtime/OpCodes/Tail.ts
@@ -0,0 +1,22 @@
+﻿module CIL.Runtime
+{
+    export class Tail extends OpCode
+    {
+        number(): number
+        {
+            return 65044;
+        }
+
+        argumentCount(): number[]
+        {
+            return [];
+        }
+
+        constructor(memory: MemorySystem.Memory, stack: StackFrame[])
+        {
+            super(memory, stack);
+        }
+    }
+
+    OpCode.opCodes[Tail.prototype.number()] = <any>Tail;
+} 
