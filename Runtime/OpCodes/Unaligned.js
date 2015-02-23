d22eb8f8d786d22a5c6c1d8d253b33c571ccc84d
Shmueli Englard <shmueli.yosef@englard.net>
2015-02-23 18:59:56 -0500

    Added unaligned opcode

---
+++ Runtime/OpCodes/Unaligned.js
@@ -0,0 +1,32 @@
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var CIL;
+(function (CIL) {
+    (function (Runtime) {
+        "use strict";
+
+        var Unaligned = (function (_super) {
+            __extends(Unaligned, _super);
+            function Unaligned(memory, stack, alignment) {
+                _super.call(this, memory, stack);
+            }
+            Unaligned.prototype.number = function () {
+                return 65042;
+            };
+
+            Unaligned.prototype.argumentCount = function () {
+                return [1];
+            };
+            return Unaligned;
+        })(Runtime.OpCode);
+        Runtime.Unaligned = Unaligned;
+
+        Runtime.OpCode.opCodes[Unaligned.prototype.number()] = Unaligned;
+    })(CIL.Runtime || (CIL.Runtime = {}));
+    var Runtime = CIL.Runtime;
+})(CIL || (CIL = {}));
+//# sourceMappingURL=Unaligned.js.map
---
+++ Runtime/OpCodes/Unaligned.ts
@@ -0,0 +1,24 @@
+﻿module CIL.Runtime
+{
+    "use strict";
+
+    export class Unaligned extends OpCode
+    {
+        number(): number
+        {
+            return 65042;
+        }
+
+        argumentCount(): number[]
+        {
+            return [1];
+        }
+
+        constructor(memory: MemorySystem.Memory, stack: StackFrame[], alignment: number)
+        {
+            super(memory, stack);
+        }
+    }
+
+    OpCode.opCodes[Unaligned.prototype.number()] = <any>Unaligned;
+} 
