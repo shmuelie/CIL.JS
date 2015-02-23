f453d81eee7df651fbeb1d2b312efc2e67d260fd
Shmueli Englard <shmueli.yosef@englard.net>
2015-02-23 18:32:45 -0500

    added readonly opcode

---
+++ Runtime/OpCodes/Readonly.js
@@ -0,0 +1,35 @@
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var CIL;
+(function (CIL) {
+    (function (Runtime) {
+        (function (OpCodes) {
+            "use strict";
+
+            var Readonly = (function (_super) {
+                __extends(Readonly, _super);
+                function Readonly(memory, stack, type) {
+                    _super.call(this, memory, stack);
+                }
+                Readonly.prototype.number = function () {
+                    return 65054;
+                };
+
+                Readonly.prototype.argumentCount = function () {
+                    return [4];
+                };
+                return Readonly;
+            })(Runtime.OpCode);
+            OpCodes.Readonly = Readonly;
+
+            Runtime.OpCode.opCodes[Readonly.prototype.number()] = Readonly;
+        })(Runtime.OpCodes || (Runtime.OpCodes = {}));
+        var OpCodes = Runtime.OpCodes;
+    })(CIL.Runtime || (CIL.Runtime = {}));
+    var Runtime = CIL.Runtime;
+})(CIL || (CIL = {}));
+//# sourceMappingURL=Readonly.js.map
---
+++ Runtime/OpCodes/Readonly.ts
@@ -0,0 +1,24 @@
+﻿module CIL.Runtime.OpCodes
+{
+    "use strict";
+
+    export class Readonly extends OpCode
+    {
+        number(): number
+        {
+            return 65054;
+        }
+
+        argumentCount(): number[]
+        {
+            return [4];
+        }
+
+        constructor(memory: MemorySystem.Memory, stack: StackFrame[], type: TypeSystem.Type)
+        {
+            super(memory, stack);
+        }
+    }
+
+    OpCode.opCodes[Readonly.prototype.number()] = <any>Readonly;
+} 
