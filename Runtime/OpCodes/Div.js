f3e736e3103bc9aa0feb2b7e7479414140b86cf6
Shmueli Englard <shmueli.yosef@englard.net>
2015-02-24 13:45:12 -0500

    Added div opcode

---
+++ Runtime/OpCodes/Div.js
@@ -0,0 +1,44 @@
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var CIL;
+(function (CIL) {
+    (function (Runtime) {
+        (function (OpCodes) {
+            var Div = (function (_super) {
+                __extends(Div, _super);
+                function Div(memory, stack) {
+                    _super.call(this, memory, stack);
+                }
+                Div.prototype.number = function () {
+                    return 91;
+                };
+
+                Div.prototype.execute = function () {
+                    var value2 = this.stack[0].values.pop();
+                    var value1 = this.stack[0].values.pop();
+
+                    if ((value1.type === 2 /* SignedInt */ || value1.type === 3 /* UnsignedInt */) && (value2.type === 2 /* SignedInt */ || value2.type === 3 /* UnsignedInt */)) {
+                        var int1 = value1.value;
+                        var int2 = value2.value;
+                        this.stack[0].values.push(new Runtime.StackFrameValue(value1.type, int1.intDivition(int2).q));
+                    } else if (value1.type === 1 /* Float */ && value2.type === 1 /* Float */) {
+                        this.stack[0].values.push(new Runtime.StackFrameValue(1 /* Float */, value1.value / value2.value));
+                    } else {
+                        throw new TypeError("add (0x58) called on operands of type " + Runtime.StackFrameValueType[value1.type] + " and " + Runtime.StackFrameValueType[value2.type] + ".");
+                    }
+                };
+                return Div;
+            })(Runtime.OpCode);
+            OpCodes.Div = Div;
+
+            Runtime.OpCode.opCodes[Div.prototype.number()] = Div;
+        })(Runtime.OpCodes || (Runtime.OpCodes = {}));
+        var OpCodes = Runtime.OpCodes;
+    })(CIL.Runtime || (CIL.Runtime = {}));
+    var Runtime = CIL.Runtime;
+})(CIL || (CIL = {}));
+//# sourceMappingURL=Div.js.map
---
+++ Runtime/OpCodes/Div.ts
@@ -0,0 +1,38 @@
+﻿module CIL.Runtime.OpCodes
+{
+    export class Div extends OpCode
+    {
+        number(): number
+        {
+            return 91;
+        }
+
+        execute(): void
+        {
+            var value2: StackFrameValue = this.stack[0].values.pop();
+            var value1: StackFrameValue = this.stack[0].values.pop();
+
+            if ((value1.type === StackFrameValueType.SignedInt || value1.type === StackFrameValueType.UnsignedInt) && (value2.type === StackFrameValueType.SignedInt || value2.type === StackFrameValueType.UnsignedInt))
+            {
+                var int1: boolean[] = value1.value;
+                var int2: boolean[] = value2.value;
+                this.stack[0].values.push(new StackFrameValue(value1.type, int1.intDivition(int2).q));
+            }
+            else if (value1.type === StackFrameValueType.Float && value2.type === StackFrameValueType.Float)
+            {
+                this.stack[0].values.push(new StackFrameValue(StackFrameValueType.Float, value1.value / value2.value));
+            }
+            else
+            {
+                throw new TypeError("add (0x58) called on operands of type " + StackFrameValueType[value1.type] + " and " + StackFrameValueType[value2.type] + ".");
+            }
+        }
+
+        constructor(memory: MemorySystem.Memory, stack: StackFrame[])
+        {
+            super(memory, stack);
+        }
+    }
+
+    OpCode.opCodes[Div.prototype.number()] = <any>Div;
+} 
