aa82e43e720b617b037baf3b52fcd91f08f53b93
Shmueli Englard <shmueli.yosef@englard.net>
2015-02-24 14:17:07 -0500

    Added ldarg.s opcode

---
+++ Runtime/OpCodes/LdargS.js
@@ -0,0 +1,35 @@
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var CIL;
+(function (CIL) {
+    (function (Runtime) {
+        (function (OpCodes) {
+            "use strict";
+
+            var LdargS = (function (_super) {
+                __extends(LdargS, _super);
+                function LdargS(memory, stack, argIndex) {
+                    _super.call(this, memory, stack, argIndex);
+                }
+                LdargS.prototype.number = function () {
+                    return 14;
+                };
+
+                LdargS.prototype.argumentCount = function () {
+                    return [1];
+                };
+                return LdargS;
+            })(OpCodes.Ldarg);
+            OpCodes.LdargS = LdargS;
+
+            Runtime.OpCode.opCodes[LdargS.prototype.number()] = LdargS;
+        })(Runtime.OpCodes || (Runtime.OpCodes = {}));
+        var OpCodes = Runtime.OpCodes;
+    })(CIL.Runtime || (CIL.Runtime = {}));
+    var Runtime = CIL.Runtime;
+})(CIL || (CIL = {}));
+//# sourceMappingURL=LdargS.js.map
---
+++ Runtime/OpCodes/LdargS.ts
@@ -0,0 +1,24 @@
+﻿module CIL.Runtime.OpCodes
+{
+    "use strict";
+
+    export class LdargS extends Ldarg
+    {
+        number(): number
+        {
+            return 14;
+        }
+
+        argumentCount(): number[]
+        {
+            return [1];
+        }
+
+        constructor(memory: MemorySystem.Memory, stack: StackFrame[], argIndex: number)
+        {
+            super(memory, stack, argIndex);
+        }
+    }
+
+    OpCode.opCodes[LdargS.prototype.number()] = <any>LdargS;
+} 
