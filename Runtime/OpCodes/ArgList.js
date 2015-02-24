463ae1f127bc3ea4ac1e5e39fedc5cd0588a22c7
Shmueli Englard <shmueli.yosef@englard.net>
2015-02-24 11:21:20 -0500

    Added opcode arglist, no implemented tthough, just stub

---
+++ Runtime/OpCodes/ArgList.js
@@ -0,0 +1,35 @@
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var CIL;
+(function (CIL) {
+    (function (Runtime) {
+        (function (OpCodes) {
+            "use strict";
+
+            var ArgList = (function (_super) {
+                __extends(ArgList, _super);
+                function ArgList(memory, stack) {
+                    _super.call(this, memory, stack);
+                }
+                ArgList.prototype.number = function () {
+                    return 65024;
+                };
+
+                ArgList.prototype.execute = function () {
+                    throw new Error("NotImplemented");
+                };
+                return ArgList;
+            })(Runtime.OpCode);
+            OpCodes.ArgList = ArgList;
+
+            Runtime.OpCode.opCodes[ArgList.prototype.number()] = ArgList;
+        })(Runtime.OpCodes || (Runtime.OpCodes = {}));
+        var OpCodes = Runtime.OpCodes;
+    })(CIL.Runtime || (CIL.Runtime = {}));
+    var Runtime = CIL.Runtime;
+})(CIL || (CIL = {}));
+//# sourceMappingURL=ArgList.js.map
---
+++ Runtime/OpCodes/ArgList.ts
@@ -0,0 +1,24 @@
+﻿module CIL.Runtime.OpCodes
+{
+    "use strict";
+
+    export class ArgList extends OpCode
+    {
+        number(): number
+        {
+            return 65024;
+        }
+
+        execute(): void
+        {
+            throw new Error("NotImplemented");
+        }
+
+        constructor(memory: MemorySystem.Memory, stack: StackFrame[])
+        {
+            super(memory, stack);
+        }
+    }
+
+    OpCode.opCodes[ArgList.prototype.number()] = <any> ArgList;
+} 
