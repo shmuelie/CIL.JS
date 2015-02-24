7dadf4605456314ed74a839e0b5d07efd02882ca
Shmueli Englard <shmueli.yosef@englard.net>
2015-02-24 13:51:32 -0500

    Added dup opcode

---
+++ Runtime/OpCodes/Dup.js
@@ -0,0 +1,36 @@
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var CIL;
+(function (CIL) {
+    (function (Runtime) {
+        (function (OpCodes) {
+            "use strict";
+
+            var Dup = (function (_super) {
+                __extends(Dup, _super);
+                function Dup(memory, stack) {
+                    _super.call(this, memory, stack);
+                }
+                Dup.prototype.number = function () {
+                    return 37;
+                };
+
+                Dup.prototype.execute = function () {
+                    var values = this.stack[0].values;
+                    values.push(values[values.length - 1]);
+                };
+                return Dup;
+            })(Runtime.OpCode);
+            OpCodes.Dup = Dup;
+
+            Runtime.OpCode.opCodes[Dup.prototype.number()] = Dup;
+        })(Runtime.OpCodes || (Runtime.OpCodes = {}));
+        var OpCodes = Runtime.OpCodes;
+    })(CIL.Runtime || (CIL.Runtime = {}));
+    var Runtime = CIL.Runtime;
+})(CIL || (CIL = {}));
+//# sourceMappingURL=Dup.js.map
---
+++ Runtime/OpCodes/Dup.ts
@@ -0,0 +1,25 @@
+﻿module CIL.Runtime.OpCodes
+{
+    "use strict";
+
+    export class Dup extends OpCode
+    {
+        number(): number
+        {
+            return 37;
+        }
+
+        execute(): void
+        {
+            var values: StackFrameValue[] = this.stack[0].values;
+            values.push(values[values.length - 1]);
+        }
+
+        constructor(memory: MemorySystem.Memory, stack: StackFrame[])
+        {
+            super(memory, stack);
+        }
+    }
+
+    OpCode.opCodes[Dup.prototype.number()] = <any>Dup;
+}
