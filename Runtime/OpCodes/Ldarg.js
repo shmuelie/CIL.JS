eff56508123c7ef17f39717c18c018d3ce1398c6
Shmueli Englard <shmueli.yosef@englard.net>
2015-02-24 14:02:24 -0500

    Added ldarg opcode

---
+++ Runtime/OpCodes/Ldarg.js
@@ -0,0 +1,40 @@
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var CIL;
+(function (CIL) {
+    (function (Runtime) {
+        (function (OpCodes) {
+            "use strict";
+
+            var Ldarg = (function (_super) {
+                __extends(Ldarg, _super);
+                function Ldarg(memory, stack, argIndex) {
+                    _super.call(this, memory, stack);
+                    this.argIndex = argIndex;
+                }
+                Ldarg.prototype.number = function () {
+                    return 65033;
+                };
+
+                Ldarg.prototype.argumentCount = function () {
+                    return [2];
+                };
+
+                Ldarg.prototype.execute = function () {
+                    this.stack[0].values.push(this.stack[0].argPointers[this.argIndex]);
+                };
+                return Ldarg;
+            })(Runtime.OpCode);
+            OpCodes.Ldarg = Ldarg;
+
+            Runtime.OpCode.opCodes[Ldarg.prototype.number()] = Ldarg;
+        })(Runtime.OpCodes || (Runtime.OpCodes = {}));
+        var OpCodes = Runtime.OpCodes;
+    })(CIL.Runtime || (CIL.Runtime = {}));
+    var Runtime = CIL.Runtime;
+})(CIL || (CIL = {}));
+//# sourceMappingURL=Ldarg.js.map
---
+++ Runtime/OpCodes/Ldarg.ts
@@ -0,0 +1,32 @@
+﻿module CIL.Runtime.OpCodes
+{
+    "use strict";
+
+    export class Ldarg extends OpCode
+    {
+        private argIndex: number;
+
+        number(): number
+        {
+            return 65033;
+        }
+
+        argumentCount(): number[]
+        {
+            return [2];
+        }
+
+        execute(): void
+        {
+            this.stack[0].values.push(this.stack[0].argPointers[this.argIndex]);
+        }
+
+        constructor(memory: MemorySystem.Memory, stack: StackFrame[], argIndex: number)
+        {
+            super(memory, stack);
+            this.argIndex = argIndex;
+        }
+    }
+
+    OpCode.opCodes[Ldarg.prototype.number()] = <any>Ldarg;
+} 
