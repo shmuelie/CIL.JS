3edd9d4452ed21caf9b5d5ee5f8716af0f55ade3
Shmueli Englard <shmueli.yosef@englard.net>
2015-02-24 10:50:02 -0500

    Added use strict

--- Runtime/integerMath.js
+++ Runtime/integerMath.js
@@ -1,195 +1,203 @@
-﻿Array.prototype.intAdd = function (other, overflowThrow) {
-    if (typeof overflowThrow === "undefined") { overflowThrow = false; }
-    var persision = Math.max(this.length, other.length);
-    var paddedThis = [];
-    while (paddedThis.length < persision) {
-        if (paddedThis.length < this.length) {
-            paddedThis.unshift(this[this.length - paddedThis.length - 1]);
-        } else {
-            paddedThis.unshift(this[0]);
-        }
-    }
+﻿var CIL;
+(function (CIL) {
+    (function (Runtime) {
+        "use strict";
 
-    var paddedOther = [];
-    while (paddedOther.length < persision) {
-        if (paddedOther.length < other.length) {
-            paddedOther.unshift(other[other.length - paddedOther.length - 1]);
-        } else {
-            paddedOther.unshift(other[0]);
-        }
-    }
-    var lastOverflow = false;
-    var overflow = false;
-    var result = [];
+        Array.prototype.intAdd = function (other, overflowThrow) {
+            if (typeof overflowThrow === "undefined") { overflowThrow = false; }
+            var persision = Math.max(this.length, other.length);
+            var paddedThis = [];
+            while (paddedThis.length < persision) {
+                if (paddedThis.length < this.length) {
+                    paddedThis.unshift(this[this.length - paddedThis.length - 1]);
+                } else {
+                    paddedThis.unshift(this[0]);
+                }
+            }
 
-    for (var i = persision; i >= 0; i--) {
-        if (paddedThis[i] && paddedOther[i] && overflow) {
-            result.unshift(true);
-        } else if (paddedThis[i] && paddedOther[i] && !overflow) {
-            result.unshift(false);
-            overflow = true;
-        } else if (!paddedThis[i] && paddedOther[i] && overflow) {
-            result.unshift(false);
-        } else if (!paddedThis[i] && paddedOther[i] && !overflow) {
-            result.unshift(true);
-        } else if (paddedThis[i] && !paddedOther[i] && overflow) {
-            result.unshift(false);
-        } else if (paddedThis[i] && !paddedOther[i] && !overflow) {
-            result.unshift(true);
-        } else if (!paddedThis[i] && !paddedOther[i] && overflow) {
-            result.unshift(true);
-            overflow = false;
-        } else if (!paddedThis[i] && !paddedOther[i] && !overflow) {
-            result.unshift(false);
-        }
-        lastOverflow = overflow;
-    }
+            var paddedOther = [];
+            while (paddedOther.length < persision) {
+                if (paddedOther.length < other.length) {
+                    paddedOther.unshift(other[other.length - paddedOther.length - 1]);
+                } else {
+                    paddedOther.unshift(other[0]);
+                }
+            }
+            var lastOverflow = false;
+            var overflow = false;
+            var result = [];
 
-    if (overflow !== lastOverflow && overflowThrow) {
-        throw new Error("OVERFLOW");
-    }
+            for (var i = persision; i >= 0; i--) {
+                if (paddedThis[i] && paddedOther[i] && overflow) {
+                    result.unshift(true);
+                } else if (paddedThis[i] && paddedOther[i] && !overflow) {
+                    result.unshift(false);
+                    overflow = true;
+                } else if (!paddedThis[i] && paddedOther[i] && overflow) {
+                    result.unshift(false);
+                } else if (!paddedThis[i] && paddedOther[i] && !overflow) {
+                    result.unshift(true);
+                } else if (paddedThis[i] && !paddedOther[i] && overflow) {
+                    result.unshift(false);
+                } else if (paddedThis[i] && !paddedOther[i] && !overflow) {
+                    result.unshift(true);
+                } else if (!paddedThis[i] && !paddedOther[i] && overflow) {
+                    result.unshift(true);
+                    overflow = false;
+                } else if (!paddedThis[i] && !paddedOther[i] && !overflow) {
+                    result.unshift(false);
+                }
+                lastOverflow = overflow;
+            }
 
-    return result;
-};
+            if (overflow !== lastOverflow && overflowThrow) {
+                throw new Error("OVERFLOW");
+            }
 
-Array.prototype.intSubtraction = function (other) {
-    var persision = Math.max(this.length, other.length);
-    var paddedThis = [];
-    while (paddedThis.length < persision) {
-        if (paddedThis.length < this.length) {
-            paddedThis.unshift(this[this.length - paddedThis.length - 1]);
-        } else {
-            paddedThis.unshift(this[0]);
-        }
-    }
+            return result;
+        };
 
-    var paddedOther = [];
-    while (paddedOther.length < persision) {
-        if (paddedOther.length < other.length) {
-            paddedOther.unshift(!other[other.length - paddedOther.length - 1]);
-        } else {
-            paddedOther.unshift(!other[0]);
-        }
-    }
+        Array.prototype.intSubtraction = function (other) {
+            var persision = Math.max(this.length, other.length);
+            var paddedThis = [];
+            while (paddedThis.length < persision) {
+                if (paddedThis.length < this.length) {
+                    paddedThis.unshift(this[this.length - paddedThis.length - 1]);
+                } else {
+                    paddedThis.unshift(this[0]);
+                }
+            }
 
-    return paddedThis.intAdd(paddedOther).intAdd([true]);
-};
+            var paddedOther = [];
+            while (paddedOther.length < persision) {
+                if (paddedOther.length < other.length) {
+                    paddedOther.unshift(!other[other.length - paddedOther.length - 1]);
+                } else {
+                    paddedOther.unshift(!other[0]);
+                }
+            }
 
-Array.prototype.intMutiplication = function (other) {
-    var persision = Math.max(this.length, other.length);
-    var paddedThis = [];
-    while (paddedThis.length < persision) {
-        if (paddedThis.length < this.length) {
-            paddedThis.unshift(this[this.length - paddedThis.length - 1]);
-        } else {
-            paddedThis.unshift(this[0]);
-        }
-    }
+            return paddedThis.intAdd(paddedOther).intAdd([true]);
+        };
 
-    var paddedOther = [];
-    while (paddedOther.length < persision) {
-        if (paddedOther.length < other.length) {
-            paddedOther.unshift(other[other.length - paddedOther.length - 1]);
-        } else {
-            paddedOther.unshift(other[0]);
-        }
-    }
+        Array.prototype.intMutiplication = function (other) {
+            var persision = Math.max(this.length, other.length);
+            var paddedThis = [];
+            while (paddedThis.length < persision) {
+                if (paddedThis.length < this.length) {
+                    paddedThis.unshift(this[this.length - paddedThis.length - 1]);
+                } else {
+                    paddedThis.unshift(this[0]);
+                }
+            }
 
-    var A = [];
-    var i;
-    for (i = 0; i < paddedThis.length; i++) {
-        A.push(paddedThis[i]);
-    }
-    while (A.length <= persision * 2 + 1) {
-        A.push(false);
-    }
+            var paddedOther = [];
+            while (paddedOther.length < persision) {
+                if (paddedOther.length < other.length) {
+                    paddedOther.unshift(other[other.length - paddedOther.length - 1]);
+                } else {
+                    paddedOther.unshift(other[0]);
+                }
+            }
 
-    var S = [];
-    for (i = 0; i < paddedThis.length; i++) {
-        S.push(!paddedThis[i]);
-    }
-    while (A.length <= persision * 2 + 1) {
-        S.push(false);
-    }
+            var A = [];
+            var i;
+            for (i = 0; i < paddedThis.length; i++) {
+                A.push(paddedThis[i]);
+            }
+            while (A.length <= persision * 2 + 1) {
+                A.push(false);
+            }
 
-    var P = [];
-    for (i = 0; i < paddedOther.length; i++) {
-        P.push(paddedOther[i]);
-    }
-    while (P.length < persision * 2 + 1) {
-        P.unshift(false);
-    }
+            var S = [];
+            for (i = 0; i < paddedThis.length; i++) {
+                S.push(!paddedThis[i]);
+            }
+            while (A.length <= persision * 2 + 1) {
+                S.push(false);
+            }
 
-    for (i = 0; i < persision; i++) {
-        var value;
-        if (!P[P.length - 2] && P[P.length - 1]) {
-            value = P.intAdd(A);
-        } else if (P[P.length - 2] && !P[P.length - 1]) {
-            value = P.intAdd(S);
-        } else {
-            value = P;
-        }
+            var P = [];
+            for (i = 0; i < paddedOther.length; i++) {
+                P.push(paddedOther[i]);
+            }
+            while (P.length < persision * 2 + 1) {
+                P.unshift(false);
+            }
 
-        value.pop();
-        value.unshift(false);
+            for (i = 0; i < persision; i++) {
+                var value;
+                if (!P[P.length - 2] && P[P.length - 1]) {
+                    value = P.intAdd(A);
+                } else if (P[P.length - 2] && !P[P.length - 1]) {
+                    value = P.intAdd(S);
+                } else {
+                    value = P;
+                }
 
-        P = value;
-    }
+                value.pop();
+                value.unshift(false);
 
-    P.pop();
+                P = value;
+            }
 
-    var result = [];
-    for (i = 0; i < persision; i++) {
-        result.unshift(P.pop());
-    }
-    return result;
-};
+            P.pop();
 
-Array.prototype.intDivition = function (other) {
-    var persision = Math.max(this.length, other.length);
-    var paddedThis = [];
-    while (paddedThis.length < persision * 2) {
-        if (paddedThis.length < this.length) {
-            paddedThis.unshift(this[this.length - paddedThis.length - 1]);
-        } else {
-            paddedThis.unshift(this[0]);
-        }
-    }
+            var result = [];
+            for (i = 0; i < persision; i++) {
+                result.unshift(P.pop());
+            }
+            return result;
+        };
 
-    var paddedOther = [];
-    while (paddedOther.length < persision) {
-        if (paddedOther.length < other.length) {
-            paddedOther.unshift(other[other.length - paddedOther.length - 1]);
-        } else {
-            paddedOther.unshift(other[0]);
-        }
-    }
+        Array.prototype.intDivition = function (other) {
+            var persision = Math.max(this.length, other.length);
+            var paddedThis = [];
+            while (paddedThis.length < persision * 2) {
+                if (paddedThis.length < this.length) {
+                    paddedThis.unshift(this[this.length - paddedThis.length - 1]);
+                } else {
+                    paddedThis.unshift(this[0]);
+                }
+            }
 
-    var R = [];
-    var Q = [];
+            var paddedOther = [];
+            while (paddedOther.length < persision) {
+                if (paddedOther.length < other.length) {
+                    paddedOther.unshift(other[other.length - paddedOther.length - 1]);
+                } else {
+                    paddedOther.unshift(other[0]);
+                }
+            }
 
-    var i;
-    for (i = 0; i < persision; i++) {
-        Q.push(paddedThis[i]);
-    }
-    for (i = 0; i < persision; i++) {
-        R.push(false);
-    }
+            var R = [];
+            var Q = [];
 
-    for (i = 0; i < persision; i++) {
-        if (Q[0]) {
-            R.shift();
-            R.push(true);
-        }
-        Q.shift();
-        Q.push(false);
-        var B = R.intSubtraction(paddedOther);
-        if (!B[0]) {
-            R = B;
-            Q[Q.length - 1] = true;
-        }
-    }
+            var i;
+            for (i = 0; i < persision; i++) {
+                Q.push(paddedThis[i]);
+            }
+            for (i = 0; i < persision; i++) {
+                R.push(false);
+            }
 
-    return { q: Q, r: R };
-};
+            for (i = 0; i < persision; i++) {
+                if (Q[0]) {
+                    R.shift();
+                    R.push(true);
+                }
+                Q.shift();
+                Q.push(false);
+                var B = R.intSubtraction(paddedOther);
+                if (!B[0]) {
+                    R = B;
+                    Q[Q.length - 1] = true;
+                }
+            }
+
+            return { q: Q, r: R };
+        };
+    })(CIL.Runtime || (CIL.Runtime = {}));
+    var Runtime = CIL.Runtime;
+})(CIL || (CIL = {}));
 //# sourceMappingURL=integerMath.js.map
--- Runtime/integerMath.ts
+++ Runtime/integerMath.ts
@@ -7,268 +7,273 @@
         q: boolean[]; r: boolean[];
     };
 }
-
-Array.prototype.intAdd = function (other: boolean[], overflowThrow: boolean = false): boolean[]
+module CIL.Runtime
 {
-    var persision: number = Math.max(this.length, other.length);
-    var paddedThis: boolean[] = [];
-    while (paddedThis.length < persision)
+    "use strict";
+
+
+    Array.prototype.intAdd = function (other: boolean[], overflowThrow: boolean = false): boolean[]
     {
-        if (paddedThis.length < this.length)
+        var persision: number = Math.max(this.length, other.length);
+        var paddedThis: boolean[] = [];
+        while (paddedThis.length < persision)
         {
-            paddedThis.unshift(this[this.length - paddedThis.length - 1]);
+            if (paddedThis.length < this.length)
+            {
+                paddedThis.unshift(this[this.length - paddedThis.length - 1]);
+            }
+            else
+            {
+                paddedThis.unshift(this[0]);
+            }
         }
-        else
-        {
-            paddedThis.unshift(this[0]);
-        }
-    }
 
-    var paddedOther: boolean[] = [];
-    while (paddedOther.length < persision)
+        var paddedOther: boolean[] = [];
+        while (paddedOther.length < persision)
+        {
+            if (paddedOther.length < other.length)
+            {
+                paddedOther.unshift(other[other.length - paddedOther.length - 1]);
+            }
+            else
+            {
+                paddedOther.unshift(other[0]);
+            }
+        }
+        var lastOverflow: boolean = false;
+        var overflow: boolean = false;
+        var result: boolean[] = [];
+
+        for (var i: number = persision; i >= 0; i--)
+        {
+            if (paddedThis[i] && paddedOther[i] && overflow)
+            {
+                result.unshift(true);
+            }
+            else if (paddedThis[i] && paddedOther[i] && !overflow)
+            {
+                result.unshift(false);
+                overflow = true;
+            }
+            else if (!paddedThis[i] && paddedOther[i] && overflow)
+            {
+                result.unshift(false);
+            }
+            else if (!paddedThis[i] && paddedOther[i] && !overflow)
+            {
+                result.unshift(true);
+            }
+            else if (paddedThis[i] && !paddedOther[i] && overflow)
+            {
+                result.unshift(false);
+            }
+            else if (paddedThis[i] && !paddedOther[i] && !overflow)
+            {
+                result.unshift(true);
+            }
+            else if (!paddedThis[i] && !paddedOther[i] && overflow)
+            {
+                result.unshift(true);
+                overflow = false;
+            }
+            else if (!paddedThis[i] && !paddedOther[i] && !overflow)
+            {
+                result.unshift(false);
+            }
+            lastOverflow = overflow;
+        }
+
+        if (overflow !== lastOverflow && overflowThrow)
+        {
+            throw new Error("OVERFLOW");
+        }
+
+        return result;
+    };
+
+    Array.prototype.intSubtraction = function (other: boolean[]): boolean[]
     {
-        if (paddedOther.length < other.length)
+        var persision: number = Math.max(this.length, other.length);
+        var paddedThis: boolean[] = [];
+        while (paddedThis.length < persision)
         {
-            paddedOther.unshift(other[other.length - paddedOther.length - 1]);
+            if (paddedThis.length < this.length)
+            {
+                paddedThis.unshift(this[this.length - paddedThis.length - 1]);
+            }
+            else
+            {
+                paddedThis.unshift(this[0]);
+            }
         }
-        else
-        {
-            paddedOther.unshift(other[0]);
-        }
-    }
-    var lastOverflow: boolean = false;
-    var overflow: boolean = false;
-    var result: boolean[] = [];
 
-    for (var i: number = persision; i >= 0; i--)
+        var paddedOther: boolean[] = [];
+        while (paddedOther.length < persision)
+        {
+            if (paddedOther.length < other.length)
+            {
+                paddedOther.unshift(!other[other.length - paddedOther.length - 1]);
+            }
+            else
+            {
+                paddedOther.unshift(!other[0]);
+            }
+        }
+
+        return paddedThis.intAdd(paddedOther).intAdd([true]);
+    };
+
+    Array.prototype.intMutiplication = function (other: boolean[]): boolean[]
     {
-        if (paddedThis[i] && paddedOther[i] && overflow)
+        var persision: number = Math.max(this.length, other.length);
+        var paddedThis: boolean[] = [];
+        while (paddedThis.length < persision)
         {
-            result.unshift(true);
+            if (paddedThis.length < this.length)
+            {
+                paddedThis.unshift(this[this.length - paddedThis.length - 1]);
+            }
+            else
+            {
+                paddedThis.unshift(this[0]);
+            }
         }
-        else if (paddedThis[i] && paddedOther[i] && !overflow)
-        {
-            result.unshift(false);
-            overflow = true;
-        }
-        else if (!paddedThis[i] && paddedOther[i] && overflow)
-        {
-            result.unshift(false);
-        }
-        else if (!paddedThis[i] && paddedOther[i] && !overflow)
-        {
-            result.unshift(true);
-        }
-        else if (paddedThis[i] && !paddedOther[i] && overflow)
-        {
-            result.unshift(false);
-        }
-        else if (paddedThis[i] && !paddedOther[i] && !overflow)
-        {
-            result.unshift(true);
-        }
-        else if (!paddedThis[i] && !paddedOther[i] && overflow)
-        {
-            result.unshift(true);
-            overflow = false;
-        }
-        else if (!paddedThis[i] && !paddedOther[i] && !overflow)
-        {
-            result.unshift(false);
-        }
-        lastOverflow = overflow;
-    }
 
-    if (overflow !== lastOverflow && overflowThrow)
+        var paddedOther: boolean[] = [];
+        while (paddedOther.length < persision)
+        {
+            if (paddedOther.length < other.length)
+            {
+                paddedOther.unshift(other[other.length - paddedOther.length - 1]);
+            }
+            else
+            {
+                paddedOther.unshift(other[0]);
+            }
+        }
+
+        var A: boolean[] = [];
+        var i: number;
+        for (i = 0; i < paddedThis.length; i++)
+        {
+            A.push(paddedThis[i]);
+        }
+        while (A.length <= persision * 2 + 1)
+        {
+            A.push(false);
+        }
+
+        var S: boolean[] = [];
+        for (i = 0; i < paddedThis.length; i++)
+        {
+            S.push(!paddedThis[i]);
+        }
+        while (A.length <= persision * 2 + 1)
+        {
+            S.push(false);
+        }
+
+        var P: boolean[] = [];
+        for (i = 0; i < paddedOther.length; i++)
+        {
+            P.push(paddedOther[i]);
+        }
+        while (P.length < persision * 2 + 1)
+        {
+            P.unshift(false);
+        }
+
+        for (i = 0; i < persision; i++)
+        {
+            var value: boolean[];
+            if (!P[P.length - 2] && P[P.length - 1])
+            {
+                value = P.intAdd(A);
+            }
+            else if (P[P.length - 2] && !P[P.length - 1])
+            {
+                value = P.intAdd(S);
+            }
+            else
+            {
+                value = P;
+            }
+
+            value.pop();
+            value.unshift(false);
+
+            P = value;
+        }
+
+        P.pop();
+
+        var result: boolean[] = [];
+        for (i = 0; i < persision; i++)
+        {
+            result.unshift(P.pop());
+        }
+        return result;
+    };
+
+    Array.prototype.intDivition = function (other: boolean[]): { q: boolean[]; r: boolean[]; }
     {
-        throw new Error("OVERFLOW");
-    }
-
-    return result;
-};
-
-Array.prototype.intSubtraction = function (other: boolean[]): boolean[]
-{
-    var persision: number = Math.max(this.length, other.length);
-    var paddedThis: boolean[] = [];
-    while (paddedThis.length < persision)
-    {
-        if (paddedThis.length < this.length)
+        var persision: number = Math.max(this.length, other.length);
+        var paddedThis: boolean[] = [];
+        while (paddedThis.length < persision * 2)
         {
-            paddedThis.unshift(this[this.length - paddedThis.length - 1]);
-        }
-        else
-        {
-            paddedThis.unshift(this[0]);
-        }
-    }
-
-    var paddedOther: boolean[] = [];
-    while (paddedOther.length < persision)
-    {
-        if (paddedOther.length < other.length)
-        {
-            paddedOther.unshift(!other[other.length - paddedOther.length - 1]);
-        }
-        else
-        {
-            paddedOther.unshift(!other[0]);
-        }
-    }
-
-    return paddedThis.intAdd(paddedOther).intAdd([true]);
-};
-
-Array.prototype.intMutiplication = function (other: boolean[]): boolean[]
-{
-    var persision: number = Math.max(this.length, other.length);
-    var paddedThis: boolean[] = [];
-    while (paddedThis.length < persision)
-    {
-        if (paddedThis.length < this.length)
-        {
-            paddedThis.unshift(this[this.length - paddedThis.length - 1]);
-        }
-        else
-        {
-            paddedThis.unshift(this[0]);
-        }
-    }
-
-    var paddedOther: boolean[] = [];
-    while (paddedOther.length < persision)
-    {
-        if (paddedOther.length < other.length)
-        {
-            paddedOther.unshift(other[other.length - paddedOther.length - 1]);
-        }
-        else
-        {
-            paddedOther.unshift(other[0]);
-        }
-    }
-
-    var A: boolean[] = [];
-    var i: number;
-    for (i = 0; i < paddedThis.length; i++)
-    {
-        A.push(paddedThis[i]);
-    }
-    while (A.length <= persision * 2 + 1)
-    {
-        A.push(false);
-    }
-
-    var S: boolean[] = [];
-    for (i = 0; i < paddedThis.length; i++)
-    {
-        S.push(!paddedThis[i]);
-    }
-    while (A.length <= persision * 2 + 1)
-    {
-        S.push(false);
-    }
-
-    var P: boolean[] = [];
-    for (i = 0; i < paddedOther.length; i++)
-    {
-        P.push(paddedOther[i]);
-    }
-    while (P.length < persision * 2 + 1)
-    {
-        P.unshift(false);
-    }
-
-    for (i = 0; i < persision; i++)
-    {
-        var value: boolean[];
-        if (!P[P.length - 2] && P[P.length - 1])
-        {
-            value = P.intAdd(A);
-        }
-        else if (P[P.length - 2] && !P[P.length - 1])
-        {
-            value = P.intAdd(S);
-        }
-        else
-        {
-            value = P;
+            if (paddedThis.length < this.length)
+            {
+                paddedThis.unshift(this[this.length - paddedThis.length - 1]);
+            }
+            else
+            {
+                paddedThis.unshift(this[0]);
+            }
         }
 
-        value.pop();
-        value.unshift(false);
-
-        P = value;
-    }
-
-    P.pop();
-
-    var result: boolean[] = [];
-    for (i = 0; i < persision; i++)
-    {
-        result.unshift(P.pop());
-    }
-    return result;
-};
-
-Array.prototype.intDivition = function (other: boolean[]): { q: boolean[]; r: boolean[]; }
-{
-    var persision: number = Math.max(this.length, other.length);
-    var paddedThis: boolean[] = [];
-    while (paddedThis.length < persision * 2)
-    {
-        if (paddedThis.length < this.length)
+        var paddedOther: boolean[] = [];
+        while (paddedOther.length < persision)
         {
-            paddedThis.unshift(this[this.length - paddedThis.length - 1]);
+            if (paddedOther.length < other.length)
+            {
+                paddedOther.unshift(other[other.length - paddedOther.length - 1]);
+            }
+            else
+            {
+                paddedOther.unshift(other[0]);
+            }
         }
-        else
-        {
-            paddedThis.unshift(this[0]);
-        }
-    }
 
-    var paddedOther: boolean[] = [];
-    while (paddedOther.length < persision)
-    {
-        if (paddedOther.length < other.length)
-        {
-            paddedOther.unshift(other[other.length - paddedOther.length - 1]);
-        }
-        else
-        {
-            paddedOther.unshift(other[0]);
-        }
-    }
+        var R: boolean[] = [];
+        var Q: boolean[] = [];
 
-    var R: boolean[] = [];
-    var Q: boolean[] = [];
-
-    var i: number;
-    for (i = 0; i < persision; i++)
-    {
-        Q.push(paddedThis[i]);
-    }
-    for (i = 0; i < persision; i++)
-    {
-        R.push(false);
-    }
-
-    for (i = 0; i < persision; i++)
-    {
-        if (Q[0])
+        var i: number;
+        for (i = 0; i < persision; i++)
         {
-            R.shift();
-            R.push(true);
+            Q.push(paddedThis[i]);
         }
-        Q.shift();
-        Q.push(false);
-        var B: boolean[] = R.intSubtraction(paddedOther);
-        if (!B[0])
+        for (i = 0; i < persision; i++)
         {
-            R = B;
-            Q[Q.length - 1] = true;
+            R.push(false);
         }
-    }
 
-    return { q: Q, r: R };
-};
+        for (i = 0; i < persision; i++)
+        {
+            if (Q[0])
+            {
+                R.shift();
+                R.push(true);
+            }
+            Q.shift();
+            Q.push(false);
+            var B: boolean[] = R.intSubtraction(paddedOther);
+            if (!B[0])
+            {
+                R = B;
+                Q[Q.length - 1] = true;
+            }
+        }
+
+        return { q: Q, r: R };
+    };
+}
