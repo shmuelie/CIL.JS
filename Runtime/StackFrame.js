ffe4ed52f8379548229d2825a0d8c8fb17696c1f
Shmueli Englard <shmueli.yosef@englard.net>
2015-02-24 11:49:57 -0500

    Added current location number, switched args to be stack values, and added method to free memory

--- Runtime/StackFrame.js
+++ Runtime/StackFrame.js
@@ -7,7 +7,21 @@
             function StackFrame() {
                 this.argPointers = [];
                 this.values = [];
+                this.nextOp = 0;
             }
+            StackFrame.prototype.free = function (memory) {
+                var i;
+                for (i = 0; i < this.argPointers.length; i++) {
+                    if (this.argPointers[i].type === 0 /* Pointer */) {
+                        memory.dereferenceObject(this.argPointers[i].value);
+                    }
+                }
+                for (i = 0; i < this.values.length; i++) {
+                    if (this.values[i].type === 0 /* Pointer */) {
+                        memory.dereferenceObject(this.values[i].value);
+                    }
+                }
+            };
             return StackFrame;
         })();
         Runtime.StackFrame = StackFrame;
--- Runtime/StackFrame.ts
+++ Runtime/StackFrame.ts
@@ -4,15 +4,36 @@
 
     export class StackFrame
     {
-        argPointers: number[];
+        argPointers: StackFrameValue[];
         values: StackFrameValue[];
         this: number;
         method: TypeSystem.TypeMethod;
+        nextOp: number;
+
+        free(memory: MemorySystem.Memory): void
+        {
+            var i: number;
+            for (i = 0; i < this.argPointers.length; i++)
+            {
+                if (this.argPointers[i].type === StackFrameValueType.Pointer)
+                {
+                    memory.dereferenceObject(this.argPointers[i].value);
+                }
+            }
+            for (i = 0; i < this.values.length; i++)
+            {
+                if (this.values[i].type === StackFrameValueType.Pointer)
+                {
+                    memory.dereferenceObject(this.values[i].value);
+                }
+            }
+        }
 
         constructor()
         {
             this.argPointers = [];
             this.values = [];
+            this.nextOp = 0;
         }
     }
 } 
