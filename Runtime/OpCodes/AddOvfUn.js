9a93b66813d86f0d3d00b7aae94240267fa9ee80
Shmueli Englard <shmueli.yosef@englard.net>
2015-02-24 11:03:04 -0500

    Added add.ovf and add.ovf.un

---
+++ Runtime/OpCodes/AddOvfUn.js
@@ -0,0 +1,29 @@
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var CIL;
+(function (CIL) {
+    (function (Runtime) {
+        (function (OpCodes) {
+            var AddOvfUn = (function (_super) {
+                __extends(AddOvfUn, _super);
+                function AddOvfUn(memory, stack) {
+                    _super.call(this, memory, stack);
+                }
+                AddOvfUn.prototype.number = function () {
+                    return 215;
+                };
+                return AddOvfUn;
+            })(OpCodes.AddOvf);
+            OpCodes.AddOvfUn = AddOvfUn;
+
+            Runtime.OpCode.opCodes[AddOvfUn.prototype.number()] = AddOvfUn;
+        })(Runtime.OpCodes || (Runtime.OpCodes = {}));
+        var OpCodes = Runtime.OpCodes;
+    })(CIL.Runtime || (CIL.Runtime = {}));
+    var Runtime = CIL.Runtime;
+})(CIL || (CIL = {}));
+//# sourceMappingURL=AddOvfUn.js.map
---
+++ Runtime/OpCodes/AddOvfUn.ts
@@ -0,0 +1,17 @@
+﻿module CIL.Runtime.OpCodes
+{
+    export class AddOvfUn extends AddOvf
+    {
+        number(): number
+        {
+            return 215;
+        }
+
+        constructor(memory: MemorySystem.Memory, stack: StackFrame[])
+        {
+            super(memory, stack);
+        }
+    }
+
+    OpCode.opCodes[AddOvfUn.prototype.number()] = <any>AddOvfUn;
+} 
