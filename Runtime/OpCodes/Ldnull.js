6dbeac2ddbf48e8cb02aecde4fab829e6e05cef2
Shmueli Englard <shmueli.yosef@englard.net>
2015-02-26 22:40:20 -0500

    Added ldnull opcode

---
+++ Runtime/OpCodes/Ldnull.js
@@ -0,0 +1,35 @@
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var CIL;
+(function (CIL) {
+    (function (Runtime) {
+        (function (OpCodes) {
+            "use strict";
+
+            var Ldnull = (function (_super) {
+                __extends(Ldnull, _super);
+                function Ldnull(memory, stack) {
+                    _super.call(this, memory, stack);
+                }
+                Ldnull.prototype.number = function () {
+                    return 20;
+                };
+
+                Ldnull.prototype.execute = function () {
+                    this.stack[0].evaluationStack.push(new Runtime.StackFrameValue(0 /* Pointer */, 0));
+                };
+                return Ldnull;
+            })(Runtime.OpCode);
+            OpCodes.Ldnull = Ldnull;
+
+            Runtime.OpCode.opCodes[Ldnull.prototype.number()] = Ldnull;
+        })(Runtime.OpCodes || (Runtime.OpCodes = {}));
+        var OpCodes = Runtime.OpCodes;
+    })(CIL.Runtime || (CIL.Runtime = {}));
+    var Runtime = CIL.Runtime;
+})(CIL || (CIL = {}));
+//# sourceMappingURL=Ldnull.js.map
---
+++ Runtime/OpCodes/Ldnull.ts
@@ -0,0 +1,24 @@
+﻿module CIL.Runtime.OpCodes
+{
+    "use strict";
+
+    export class Ldnull extends OpCode
+    {
+        number(): number
+        {
+            return 20;
+        }
+
+        execute(): void
+        {
+            this.stack[0].evaluationStack.push(new StackFrameValue(StackFrameValueType.Pointer, 0));
+        }
+
+        constructor(memory: MemorySystem.Memory, stack: StackFrame[])
+        {
+            super(memory, stack);
+        }
+    }
+
+    OpCode.opCodes[Ldnull.prototype.number()] = <any>Ldnull;
+} 
