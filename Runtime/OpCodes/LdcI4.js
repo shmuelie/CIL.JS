a4988bb13f3c32a973eb641a5f819b3359660cc9
Shmueli Englard <shmueli.yosef@englard.net>
2015-03-04 15:58:08 -0500

    Added Ldc.i4 opcode

---
+++ Runtime/OpCodes/LdcI4.js
@@ -0,0 +1,40 @@
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var CIL;
+(function (CIL) {
+    (function (Runtime) {
+        (function (OpCodes) {
+            "use strict";
+
+            var LdcI4 = (function (_super) {
+                __extends(LdcI4, _super);
+                function LdcI4(memory, stack, num) {
+                    _super.call(this, memory, stack);
+                    this.num = num;
+                }
+                LdcI4.prototype.number = function () {
+                    return 32;
+                };
+
+                LdcI4.prototype.argumentCount = function () {
+                    return [4];
+                };
+
+                LdcI4.prototype.execute = function () {
+                    this.stack[0].evaluationStack.push(new Runtime.StackFrameValue(2 /* SignedInt */, this.num));
+                };
+                return LdcI4;
+            })(Runtime.OpCode);
+            OpCodes.LdcI4 = LdcI4;
+
+            Runtime.OpCode.opCodes[LdcI4.prototype.number()] = LdcI4;
+        })(Runtime.OpCodes || (Runtime.OpCodes = {}));
+        var OpCodes = Runtime.OpCodes;
+    })(CIL.Runtime || (CIL.Runtime = {}));
+    var Runtime = CIL.Runtime;
+})(CIL || (CIL = {}));
+//# sourceMappingURL=LdcI4.js.map
---
+++ Runtime/OpCodes/LdcI4.ts
@@ -0,0 +1,32 @@
+﻿module CIL.Runtime.OpCodes
+{
+    "use strict";
+
+    export class LdcI4 extends OpCode
+    {
+        private num: boolean[];
+
+        number(): number
+        {
+            return 32;
+        }
+
+        argumentCount(): number[]
+        {
+            return [4];
+        }
+
+        execute(): void
+        {
+            this.stack[0].evaluationStack.push(new StackFrameValue(StackFrameValueType.SignedInt, this.num));
+        }
+
+        constructor(memory: MemorySystem.Memory, stack: StackFrame[], num: boolean[])
+        {
+            super(memory, stack);
+            this.num = num;
+        }
+    }
+
+    OpCode.opCodes[LdcI4.prototype.number()] = <any>LdcI4;
+} 
