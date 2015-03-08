905b11cd6491304a14f6ac88abc91b1d4f980e7d
Shmueli Englard <shmueli.yosef@englard.net>
2015-03-08 01:38:18 -0500

    Finished integer tests

--- Tests/integerTests.js
+++ Tests/integerTests.js
@@ -5,29 +5,73 @@ var CIL;
     (function (Tests) {
         "use strict";
 
-        function compare(first, second) {
-            if (first.length !== second.length) {
-                return false;
-            }
-
-            var length = first.length;
-            for (var i = 0; i < length; i++) {
-                if (first[i] !== second[i]) {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
         QUnit.module("Integer Tests");
 
         QUnit.test("round trip", function (assert) {
             var value = [128, 55, 44, 55];
             var i = Integer.fromBytes(value);
             var comp = i.toBytes();
-            assert.strictEqual(comp.length, value.length, "Should have the same number of bytes");
-            assert.ok(compare(value, comp), "Byte arrays should be equal");
+            assert.propEqual(comp, value, "round trip should result in equal arrays");
+        });
+
+        QUnit.test("byte array to bit array", function (assert) {
+            var bytes = [0x07, 0x5B, 0xCD, 0x15];
+            var bits = [false, false, false, false, false, true, true, true, false, true, false, true, true, false, true, true, true, true, false, false, true, true, false, true, false, false, false, true, false, true, false, true];
+            var i = Integer.fromBytes(bytes);
+            assert.propEqual(i.bits, bits, "conversion to bit array should result in expected bit array");
+        });
+
+        QUnit.test("addition", function (assert) {
+            var bytes1 = [0x07, 0x5B, 0xCD, 0x15];
+            var bytes2 = [0x07, 0x5B, 0xCD, 0x15];
+            var int1 = Integer.fromBytes(bytes1);
+            var int2 = Integer.fromBytes(bytes2);
+            var int3 = int1.add(int2);
+            var result = int3.toBytes();
+            var expected = [0x0E, 0xB7, 0x9A, 0x2A];
+            assert.propEqual(result, expected, "adding two integers should result in expected integer");
+        });
+
+        QUnit.test("subtraction", function (assert) {
+            var bytes1 = [0x07, 0x5B, 0xCD, 0x15];
+            var bytes2 = [0x07, 0x5B, 0xCD, 0x16];
+            var int1 = Integer.fromBytes(bytes1);
+            var int2 = Integer.fromBytes(bytes2);
+            var int3 = int1.subtract(int2);
+            var result = int3.toBytes();
+            var expected = [0xFF, 0xFF, 0xFF, 0xFF];
+            assert.propEqual(result, expected, "subtracting two integers should result in the expected integer");
+        });
+
+        QUnit.test("multiplication", function (assert) {
+            var bytes1 = [0x07, 0x5B, 0xCD, 0x15];
+            var bytes2 = [0x00, 0x00, 0x00, 0x02];
+            var int1 = Integer.fromBytes(bytes1);
+            var int2 = Integer.fromBytes(bytes2);
+            var int3 = int1.mutliply(int2);
+            var result = int3.toBytes();
+            var expected = [0x0E, 0xB7, 0x9A, 0x2A];
+            assert.propEqual(result, expected, "multipling two integers should result in the expected integer");
+        });
+
+        QUnit.test("division", function (assert) {
+            var bytes1 = [0x0E, 0xB7, 0x9A, 0x2A];
+            var bytes2 = [0x00, 0x00, 0x00, 0x02];
+            var int1 = Integer.fromBytes(bytes1);
+            var int2 = Integer.fromBytes(bytes2);
+            var int3 = int1.division(int2).q;
+            var result = int3.toBytes();
+            var expected = [0x07, 0x5B, 0xCD, 0x15];
+            assert.propEqual(result, expected, "division of two integers should result in the expected integer");
+        });
+
+        QUnit.test("negate", function (assert) {
+            var bytes = [0x07, 0x5B, 0xCD, 0x15];
+            var int1 = Integer.fromBytes(bytes);
+            var int2 = int1.negate();
+            var result = int2.toBytes();
+            var expected = [0xF8, 0xA4, 0x32, 0xEB];
+            assert.propEqual(result, expected, "negating an integer should result in the expected integer");
         });
     })(CIL.Tests || (CIL.Tests = {}));
     var Tests = CIL.Tests;
--- Tests/integerTests.ts
+++ Tests/integerTests.ts
@@ -4,25 +4,6 @@ module CIL.Tests
 {
     "use strict";
 
-    function compare<T>(first: T[], second: T[]): boolean
-    {
-        if (first.length !== second.length)
-        {
-            return false;
-        }
-
-        var length: number = first.length;
-        for (var i: number = 0; i < length; i++)
-        {
-            if (first[i] !== second[i])
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
     QUnit.module("Integer Tests");
 
     QUnit.test("round trip", function (assert: QUnitAssert)
@@ -30,7 +11,72 @@ module CIL.Tests
         var value: number[] = [128, 55, 44, 55];
         var i: Integer = Integer.fromBytes(value);
         var comp: number[] = i.toBytes();
-        assert.strictEqual(comp.length, value.length, "Should have the same number of bytes");
-        assert.ok(compare<number>(value, comp), "Byte arrays should be equal");
+        assert.propEqual(comp, value, "round trip should result in equal arrays");
+    });
+
+    QUnit.test("byte array to bit array", function (assert: QUnitAssert)
+    {
+        var bytes: number[] = [0x07, 0x5B, 0xCD, 0x15];
+        var bits: boolean[] = [false, false, false, false, false, true, true, true, false, true, false, true, true, false, true, true, true, true, false, false, true, true, false, true, false, false, false, true, false, true, false, true];
+        var i: Integer = Integer.fromBytes(bytes);
+        assert.propEqual(i.bits, bits, "conversion to bit array should result in expected bit array");
+    });
+
+    QUnit.test("addition", function (assert: QUnitAssert)
+    {
+        var bytes1: number[] = [0x07, 0x5B, 0xCD, 0x15];
+        var bytes2: number[] = [0x07, 0x5B, 0xCD, 0x15];
+        var int1: Integer = Integer.fromBytes(bytes1);
+        var int2: Integer = Integer.fromBytes(bytes2);
+        var int3: Integer = int1.add(int2);
+        var result: number[] = int3.toBytes();
+        var expected: number[] = [0x0E, 0xB7, 0x9A, 0x2A];
+        assert.propEqual(result, expected, "adding two integers should result in expected integer");
+    });
+
+    QUnit.test("subtraction", function (assert: QUnitAssert)
+    {
+        var bytes1: number[] = [0x07, 0x5B, 0xCD, 0x15];
+        var bytes2: number[] = [0x07, 0x5B, 0xCD, 0x16];
+        var int1: Integer = Integer.fromBytes(bytes1);
+        var int2: Integer = Integer.fromBytes(bytes2);
+        var int3: Integer = int1.subtract(int2);
+        var result: number[] = int3.toBytes();
+        var expected: number[] = [0xFF, 0xFF, 0xFF, 0xFF];
+        assert.propEqual(result, expected, "subtracting two integers should result in the expected integer");
+    });
+
+    QUnit.test("multiplication", function (assert: QUnitAssert)
+    {
+        var bytes1: number[] = [0x07, 0x5B, 0xCD, 0x15];
+        var bytes2: number[] = [0x00, 0x00, 0x00, 0x02];
+        var int1: Integer = Integer.fromBytes(bytes1);
+        var int2: Integer = Integer.fromBytes(bytes2);
+        var int3: Integer = int1.mutliply(int2);
+        var result: number[] = int3.toBytes();
+        var expected: number[] = [0x0E, 0xB7, 0x9A, 0x2A];
+        assert.propEqual(result, expected, "multipling two integers should result in the expected integer");
+    });
+
+    QUnit.test("division", function (assert: QUnitAssert)
+    {
+        var bytes1: number[] = [0x0E, 0xB7, 0x9A, 0x2A];
+        var bytes2: number[] = [0x00, 0x00, 0x00, 0x02];
+        var int1: Integer = Integer.fromBytes(bytes1);
+        var int2: Integer = Integer.fromBytes(bytes2);
+        var int3: Integer = int1.division(int2).q;
+        var result: number[] = int3.toBytes();
+        var expected: number[] = [0x07, 0x5B, 0xCD, 0x15];
+        assert.propEqual(result, expected, "division of two integers should result in the expected integer");
+    });
+
+    QUnit.test("negate", function (assert: QUnitAssert)
+    {
+        var bytes: number[] = [0x07, 0x5B, 0xCD, 0x15];
+        var int1: Integer = Integer.fromBytes(bytes);
+        var int2: Integer = int1.negate();
+        var result: number[] = int2.toBytes();
+        var expected: number[] = [0xF8, 0xA4, 0x32, 0xEB];
+        assert.propEqual(result, expected, "negating an integer should result in the expected integer");
     });
 }
