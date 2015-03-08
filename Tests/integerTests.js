1d7ec0c3e6467fc205db87ac1dc15476f8ca20c4
Shmueli Englard <shmueli.yosef@englard.net>
2015-03-07 22:27:10 -0500

    Added starting of TESTS!

---
+++ Tests/integerTests.js
@@ -0,0 +1,35 @@
+﻿var Integer = CIL.Runtime.Integer;
+
+var CIL;
+(function (CIL) {
+    (function (Tests) {
+        "use strict";
+
+        function compare(first, second) {
+            if (first.length !== second.length) {
+                return false;
+            }
+
+            var length = first.length;
+            for (var i = 0; i < length; i++) {
+                if (first[i] !== second[i]) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        QUnit.module("Integer Tests");
+
+        QUnit.test("round trip", function (assert) {
+            var value = [128, 55, 44, 55];
+            var i = Integer.fromBytes(value);
+            var comp = i.toBytes();
+            assert.strictEqual(comp.length, value.length, "Should have the same number of bytes");
+            assert.ok(compare(value, comp), "Byte arrays should be equal");
+        });
+    })(CIL.Tests || (CIL.Tests = {}));
+    var Tests = CIL.Tests;
+})(CIL || (CIL = {}));
+//# sourceMappingURL=integerTests.js.map
---
+++ Tests/integerTests.ts
@@ -0,0 +1,36 @@
+﻿import Integer = CIL.Runtime.Integer;
+
+module CIL.Tests
+{
+    "use strict";
+
+    function compare<T>(first: T[], second: T[]): boolean
+    {
+        if (first.length !== second.length)
+        {
+            return false;
+        }
+
+        var length: number = first.length;
+        for (var i: number = 0; i < length; i++)
+        {
+            if (first[i] !== second[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    QUnit.module("Integer Tests");
+
+    QUnit.test("round trip", function (assert: QUnitAssert)
+    {
+        var value: number[] = [128, 55, 44, 55];
+        var i: Integer = Integer.fromBytes(value);
+        var comp: number[] = i.toBytes();
+        assert.strictEqual(comp.length, value.length, "Should have the same number of bytes");
+        assert.ok(compare<number>(value, comp), "Byte arrays should be equal");
+    });
+}
