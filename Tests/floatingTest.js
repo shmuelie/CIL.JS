01634592d9fb754a2ad0811da0283f5ce81496ca
Shmueli Englard <shmueli.yosef@englard.net>
2015-03-08 22:04:05 -0400

    Added test for reading floating point

---
+++ Tests/floatingTest.js
@@ -0,0 +1,22 @@
+﻿var CIL;
+(function (CIL) {
+    (function (Tests) {
+        "use strict";
+
+        QUnit.module("Floating Point");
+
+        QUnit.test("from bytes 64", function (assert) {
+            var bytes = [0x40, 0x0C, 0, 0, 0, 0, 0, 0];
+            var num = CIL.Runtime.NumberHelper.parse64(bytes, 0);
+            assert.strictEqual(num, 3.5, "conversion from bytes should result in expected floating point number");
+        });
+
+        QUnit.test("from bytes 32", function (assert) {
+            var bytes = [0x40, 0x60, 0, 0];
+            var num = CIL.Runtime.NumberHelper.parse32(bytes, 0);
+            assert.strictEqual(num, 3.5, "conversion from bytes should result in expected floating point number");
+        });
+    })(CIL.Tests || (CIL.Tests = {}));
+    var Tests = CIL.Tests;
+})(CIL || (CIL = {}));
+//# sourceMappingURL=floatingTest.js.map
---
+++ Tests/floatingTest.ts
@@ -0,0 +1,20 @@
+﻿module CIL.Tests
+{
+    "use strict";
+
+    QUnit.module("Floating Point");
+
+    QUnit.test("from bytes 64", function (assert: QUnitAssert)
+    {
+        var bytes: number[] = [0x40, 0x0C, 0, 0, 0, 0, 0, 0];
+        var num: number = Runtime.NumberHelper.parse64(bytes, 0);
+        assert.strictEqual(num, 3.5, "conversion from bytes should result in expected floating point number");
+    });
+
+    QUnit.test("from bytes 32", function (assert: QUnitAssert)
+    {
+        var bytes: number[] = [0x40, 0x60, 0, 0];
+        var num: number = Runtime.NumberHelper.parse32(bytes, 0);
+        assert.strictEqual(num, 3.5, "conversion from bytes should result in expected floating point number");
+    });
+} 
