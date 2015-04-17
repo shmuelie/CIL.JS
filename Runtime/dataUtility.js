49802209f7ec77ea6f3b93e296528ffc0dfe052e
Shmueli Englard <shmueli.yosef@englard.net>
2015-04-16 23:16:41 -0400

    Cleaning up unused files

--- Runtime/dataUtility.js
+++
@@ -1,8 +0,0 @@
-﻿var CIL;
-(function (CIL) {
-    (function (Runtime) {
-        "use strict";
-    })(CIL.Runtime || (CIL.Runtime = {}));
-    var Runtime = CIL.Runtime;
-})(CIL || (CIL = {}));
-//# sourceMappingURL=dataUtility.js.map
