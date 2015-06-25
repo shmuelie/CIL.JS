4dc504cd135dd8d80c2acf558038026c98282e76
Shmueli Englard <shmueli.yosef@englard.net>
2015-06-24 21:44:05 -0400

    Finished implementing Image

--- TypeSystem/Cecil/PE/Image.js
+++ TypeSystem/Cecil/PE/Image.js
@@ -17,8 +17,45 @@
 
                 Image.prototype.getCodeIndexSize = function (codedIndex) {
                     var size = this.codedIndexSizes[codedIndex];
-                    if (size) {
+                    if (size !== null) {
+                        return size;
                     }
+                    size = Cecil.Metadata.CodedIndex.getSize(codedIndex, this.counter);
+                    return size;
+                };
+
+                Image.prototype.resolveVirtualAddress = function (rva) {
+                    var section = this.getSectionAtVirtualAddress(rva);
+                    if (section === null) {
+                        throw new Error("ArgumentOutOfRangeException");
+                    }
+                    return this.resolveVirtualAddressInSection(rva, section);
+                };
+
+                Image.prototype.resolveVirtualAddressInSection = function (rva, section) {
+                    return rva + section.pointerToRawData - section.virtualAddress;
+                };
+
+                Image.prototype.getSection = function (name) {
+                    var sections = this.sections;
+                    for (var i = 0; i < sections.length; i++) {
+                        var section = sections[i];
+                        if (section.name === name) {
+                            return section;
+                        }
+                    }
+                    return null;
+                };
+
+                Image.prototype.getSectionAtVirtualAddress = function (rva) {
+                    var sections = this.sections;
+                    for (var i = 0; i < sections.length; i++) {
+                        var section = sections[i];
+                        if (rva >= section.virtualAddress && rva < section.virtualAddress + section.sizeOfRawData) {
+                            return section;
+                        }
+                    }
+                    return null;
                 };
                 return Image;
             })();
--- TypeSystem/Cecil/PE/Image.ts
+++ TypeSystem/Cecil/PE/Image.ts
@@ -53,7 +53,51 @@
             {
                 return size;
             }
-            size = Metadata.CodedIndex.
+            size = Metadata.CodedIndex.getSize(codedIndex, this.counter);
+            return size;
+        }
+
+        resolveVirtualAddress(rva: number): number
+        {
+            var section: Section = this.getSectionAtVirtualAddress(rva);
+            if (section === null)
+            {
+                throw new Error("ArgumentOutOfRangeException");
+            }
+            return this.resolveVirtualAddressInSection(rva, section);
+        }
+
+        resolveVirtualAddressInSection(rva: number, section: Section): number
+        {
+            return rva + section.pointerToRawData - section.virtualAddress;
+        }
+
+        getSection(name: string): Section
+        {
+            var sections: Section[] = this.sections;
+            for (var i: number = 0; i < sections.length; i++)
+            {
+                var section: Section = sections[i];
+                if (section.name === name)
+                {
+                    return section;
+                }
+            }
+            return null;
+        }
+
+        getSectionAtVirtualAddress(rva: number): Section
+        {
+            var sections: Section[] = this.sections;
+            for (var i: number = 0; i < sections.length; i++)
+            {
+                var section: Section = sections[i];
+                if (rva >= section.virtualAddress && rva < section.virtualAddress + section.sizeOfRawData)
+                {
+                    return section;
+                }
+            }
+            return null;
         }
     }
 } 
